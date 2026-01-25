bf980b7dee3ec3f0f7467763fdcd382139decc65
derek cobain silva <derek.cobaindev@gmail.com>
2026-01-25 00:39:29 -0300

    fix: remove base URL para corrigir carregamento de imagens

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cobainderek.github.io',
-  base: '/serralheriadailha',
   integrations: [
     tailwind(),
     react()
